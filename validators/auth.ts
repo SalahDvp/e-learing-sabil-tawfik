b0b69b250e3b7e0346c90684b8bdc286222b0868
Salah SAID <105821405+SalahDvp@users.noreply.github.com>
2024-09-17 10:23:54 +0100

    Update auth.ts

--- validators/auth.ts
+++ validators/auth.ts
@@ -1,33 +1,103 @@
 import { z } from 'zod';
 import { parseISO, isBefore, startOfToday, startOfYear } from 'date-fns';
-export const SubjectsSchema = z.array(z.object({
-  id:z.string(),
-  subject: z.string().min(1, { message: "Subject is required" }),
-  name: z.string().min(1, { message: "Group name is required" }),
-  time: z.string().min(1, { message: "Time is required" }),
-  cs:z.string(),
-  index:z.number(),
-  group:z.string(),
-  day: z.string().min(1, { message: "Day is required" }), // e.g., Monday, Tuesday
-  start: z.string().regex(/^([01]\d|2[0-3]):?([0-5]\d)$/, { message: "Invalid start time format" }), // e.g., 14:00
-  end: z.string().regex(/^([01]\d|2[0-3]):?([0-5]\d)$/, { message: "Invalid end time format" }) // e.g., 16:00
+
+const GroupSchema = z.array(
+  z.object({
+    day: z.string(),
+    end: z.string(),
+    room: z.string(),
+    start: z.string(),
+  })
+);
+const ClassesUIDsSchema = z.array(z.object({
+  id: z.string(),
+  group: z.string(),
 }));
-// Define the Zod schema
+
+export const SubjectsSchema = z.array(
+  z.object({
+    amount: z.number(),
+    cs: z.string(),
+    debt: z.number(),
+    group: z.string(),
+    groups: GroupSchema, // Referencing the GroupSchema
+    id: z.string(),
+    index: z.number(),
+    name: z.string(),
+    sessionsLeft: z.number(),
+    sessionsToStudy: z.number(),
+    subject: z.string(),
+  })
+);
+
+const ActionTrackSchema = 
+  z.object({
+    action: z.string().optional(),
+    additionalInfo: z.object({
+      classId: z.string().optional(),
+      studentID: z.string().optional(),
+      studentName: z.string().optional(),
+      resourceId: z.string().optional(),
+      resourceType: z.string().optional(),
+      birthdate: z.date().optional(),
+      birthplace: z.string().optional(),
+    }),
+    timestamp: z.string().optional(),
+    userId: z.string().optional(),
+    userType: z.string().optional(),
+  });
+
+// Define the StudentSchema
 export const StudentSchema = z.object({
-  id:z.string(),
-  name: z.string().min(1, { message: "Name is required" }),
-  birthdate: z.date().refine((value:Date) => value < new Date(), { message: 'Please enter a valid date of birth.' }),
-  birthplace: z.string().min(1, { message: "Birthplace is required" }),
-  school: z.string().min(1, { message: "School is required" }),
-  year: z.string().min(1, { message: "Year is required" }),
-  field: z.string().min(1, { message: "Field is required" }),
-  phoneNumber:z.string().min(10, "Please enter a value between 10 and 15 characters.").max(15, "Please enter a value between 10 and 15 characters."),
-  photo:z.string().nullable(),
-  classes:SubjectsSchema,
-  studentIndex:z.number(),
-  classesUIDs:z.array(z.object({id:z.string(),group:z.string()}))
+  actionTrack: z.array(ActionTrackSchema).optional(),// Referencing the ActionTrackSchema
+  birthdate: z.date().refine((value: Date) => value < new Date(), {
+    message: 'Please enter a valid date of birth.',
+  }),
+  birthplace: z.string().min(1, { message: 'Birthplace is required' }),
+  classes: SubjectsSchema,
+  classesUIDs: ClassesUIDsSchema,
+  debt: z.number().optional(),
+  field: z.string().min(1, { message: 'Field is required' }),
+  id: z.string( { message: 'Name is required' }),
+  label:z.string().optional(),
+  lastPaymentDate: z.date().optional(),
+  monthlypayment: z.number().optional(),
+  name: z.string().min(1, { message: 'Name is required' }),
+  nextPaymentDate: z.date().optional(),
+  phoneNumber: z.string().min(10, 'Please enter a value between 10 and 15 characters.').max(15, 'Please enter a value between 10 and 15 characters.'),
+  photo: z.string().optional(),
+  registrationAndInsuranceFee: z.string( { message: 'Name is required' }),
+  school: z.string().min(1, { message: 'School is required' }),
+  student:z.string().optional(),
+  studentIndex: z.number( { message: 'Name is required' }),
+  totalAmount: z.number().optional(),
+  value:z.string().optional(),
+  year: z.string().min(1, { message: 'Year is required' }),
+  newId:z.string().optional(),
+
+
+
+
 });
 
 // Generate TypeScript type from the Zod schema
 export type Student = z.infer<typeof StudentSchema>;
 
+
+
+/**
+ * 
+  debt: z.number( { message: 'Name is required' }),
+ 
+  lastPaymentDate: z.date( { message: 'Name is required' }),
+  monthlypayment: z.number( { message: 'Name is required' }),
+  nextPaymentDate: z.date( { message: 'Name is required' }),
+  totalAmount: z.number( { message: 'Name is required' }),
+  newId:z.string().optional(),
+
+
+classes
+
+      nextPaymentDate: z.date(),
+
+ */
